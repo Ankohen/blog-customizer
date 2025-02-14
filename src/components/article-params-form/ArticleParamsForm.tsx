61c4dd5a3b91ecc78f2cbc214a4e0fe051b9774c
Stepan Fisenko <stepkafisenko@yandex.ru>
2025-02-14 17:09:58 +0300

    submitting work for review

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,160 @@
+// ArticleParamsForm.tsx
+import { useState, useRef, SyntheticEvent } from 'react';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-
+import { useClose } from 'src/components/hooks/useClose';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Spacing } from 'src/ui/Spacing';
+import { Separator } from 'src/ui/separator';
+import { Select } from 'src/ui/select';
+import { Text } from 'src/ui/text';
+import {
+	OptionType,
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	defaultArticleState,
+	ArticleStateType,
+} from 'src/constants/articleProps';
+import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+export type StateArcticleParamsForm = 'open' | 'close';
+
+export type ArticleParamsFormProps = {
+	onChange: React.Dispatch<React.SetStateAction<ArticleStateType>>;
+};
+
+export const ArticleParamsForm = ({ onChange }: ArticleParamsFormProps) => {
+	const defaultStateForm = useRef<ArticleStateType>(defaultArticleState);
+	const asideRef = useRef<HTMLDivElement | null>(null);
+
+	const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false);
+
+	const [fontFamily, setFontFamily] = useState<OptionType>(
+		defaultStateForm.current.fontFamilyOption
+	);
+	const [fontSize, setFontSize] = useState<OptionType>(
+		defaultStateForm.current.fontSizeOption
+	);
+	const [backgroundColor, setBackgroundColor] = useState<OptionType>(
+		defaultStateForm.current.backgroundColor
+	);
+	const [fontColor, setFontColor] = useState<OptionType>(
+		defaultStateForm.current.fontColor
+	);
+	const [contentWidth, setContentWidth] = useState<OptionType>(
+		defaultStateForm.current.contentWidth
+	);
+
+	useClose({
+		isOpen: isOpenMenu,
+		onClose: () => setIsOpenMenu(false),
+		rootRef: asideRef,
+	});
+
+	const toggleStateMenu = () => {
+		setIsOpenMenu((prev) => !prev);
+	};
+
+	const changefontFamily = (option: OptionType) => {
+		setFontFamily(option);
+	};
+	const changeFontSize = (option: OptionType) => {
+		setFontSize(option);
+	};
+	const changeBackgroundColor = (option: OptionType) => {
+		setBackgroundColor(option);
+	};
+	const changeFontColor = (option: OptionType) => {
+		setFontColor(option);
+	};
+	const changeContentWidth = (option: OptionType) => {
+		setContentWidth(option);
+	};
+
+	const handleOnSubmitForm = (e: SyntheticEvent) => {
+		e.preventDefault();
+		onChange({
+			fontFamilyOption: fontFamily,
+			fontColor: fontColor,
+			backgroundColor: backgroundColor,
+			contentWidth: contentWidth,
+			fontSizeOption: fontSize,
+		});
+	};
+	const handleOnClickButtonReset = () => {
+		onChange(defaultStateForm.current);
+
+		setFontFamily(defaultStateForm.current.fontFamilyOption);
+		setFontSize(defaultStateForm.current.fontSizeOption);
+		setBackgroundColor(defaultStateForm.current.backgroundColor);
+		setFontColor(defaultStateForm.current.fontColor);
+		setContentWidth(defaultStateForm.current.contentWidth);
+	};
+
 	return (
-		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+		<div ref={asideRef}>
+			<ArrowButton onClick={toggleStateMenu} isOpenMenu={isOpenMenu} />
+			<aside
+				className={clsx(styles.container, {
+					[styles.container_open]: isOpenMenu,
+				})}>
+				<form className={styles.form} onSubmit={handleOnSubmitForm}>
+					<Text as='h2' size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+					<Spacing size={50} />
+					<Select
+						options={fontFamilyOptions}
+						selected={fontFamily}
+						onChange={changefontFamily}
+						title='шрифт'
+					/>
+					<Spacing size={50} />
+					<RadioGroup
+						name='font-size'
+						options={fontSizeOptions}
+						selected={fontSize}
+						onChange={changeFontSize}
+						title='размер шрифта'
+					/>
+					<Spacing size={50} />
+					<Select
+						options={fontColors}
+						selected={fontColor}
+						onChange={changeFontColor}
+						title='цвет шрифта'
+					/>
+					<Spacing size={50} />
+					<Separator />
+					<Spacing size={50} />
+					<Select
+						options={backgroundColors}
+						selected={backgroundColor}
+						onChange={changeBackgroundColor}
+						title='цвет фона'
+					/>
+					<Spacing size={50} />
+					<Select
+						options={contentWidthArr}
+						selected={contentWidth}
+						onChange={changeContentWidth}
+						title='ширина контента'
+					/>
+					<Spacing size={207} />
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							type='reset'
+							onClick={handleOnClickButtonReset}
+						/>
+						<Button title='Применить' type='submit' />
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
